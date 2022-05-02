32e572d30e7891231e3c0546e668e7959705eda8
Filip Rudol <filiprudol@seznam.cz>
2022-05-02 12:02:57 +0200

    kk

--- fibo.py
+++ fibo.py
@@ -1,7 +1,15 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
-    pass
-
+    nth = recursive_nth_fibo(6)
+    return nth
 
 if __name__ == "__main__":
-    main()
+    print(main())
